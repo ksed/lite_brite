56e37f6d5ae9964b08c28cd133ed07a7a3efcd6a
ksed <hydrogeo.coder@gmail.com>
2016-10-05 19:48:13 -0400

    created initial grid

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,16 @@
 (function() {
-  'use strict';
-  var size = prompt('What size shirt do you want?');
+  var numberOfRows = 40; // number of rows in the grid
+  var numberOfCols = 40; // number of columns in the grid
+  var canvas = $('#canvas'); // my placement area - think of paper in drawing
+
+  // make rows and put them in the body
+  for(var rowCount = 0; rowCount < numberOfRows; rowCount += 1) {
+    var row = $(' <tr></tr>');
+    for (var colCount = 0; colCount < numberOfCols; colCount += 1) {
+      var column = $('<td></td>');
+      column.addClass('cell');
+      row.append(column);
+    }
+    canvas.append(row);
+  }
 }());
